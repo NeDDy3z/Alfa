ef547dada39ef925bf552afc99de237797de3bc1
Erik Vaněk <erikvanek0@gmail.com>
2024-01-09 07:50:20 +0100

    stuff

---
+++ Alfa/ScheduleGeneratorTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Alfa.Tests
+{
+    [TestFixture]
+    public class ScheduleGeneratorTests
+    {
+        [Test]
+        public void Generate_SchedulesGeneratedAndEvaluated()
+        {
+            // Arrange
+            var ratedSchedules = new List<Schedule>();
+            var subjects = new List<Subject>
+            {
+                // Add sample subjects as needed
+            };
+
+            var generator = new ScheduleGenerator(ratedSchedules, subjects, 2, 5);
+
+            // Act
+            generator.Generate();
+
+            // Assert
+            Assert.Greater(generator.GeneratedCount, 0);
+            Assert.IsNotEmpty(ratedSchedules);
+            // Add more specific assertions based on the expected behavior of the generator
+        }
+
+        [Test]
+        public void Evaluate_ValidSchedule_AddsToRatedSchedules()
+        {
+            // Arrange
+            var ratedSchedules = new List<Schedule>();
+            var subjects = new List<Subject>
+            {
+                // Add sample subjects as needed
+            };
+
+            var generator = new ScheduleGenerator(ratedSchedules, subjects, 2, 5);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            generator.Generate();
+            generator.Evaluate(cancellationTokenSource.Token);
+
+            // Assert
+            Assert.IsNotEmpty(ratedSchedules);
+            // Add more specific assertions based on the expected behavior of the evaluator
+        }
+
+        [Test]
+        public void Evaluate_NoGeneratedSchedule_HandlesException()
+        {
+            // Arrange
+            var ratedSchedules = new List<Schedule>();
+            var subjects = new List<Subject>
+            {
+                // Add sample subjects as needed
+            };
+
+            var generator = new ScheduleGenerator(ratedSchedules, subjects, 2, 5);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => generator.Evaluate(cancellationTokenSource.Token));
+        }
+
+        // Add more tests as needed, such as testing cancellation, timeout, etc.
+    }
+}
